0678710cea4139026785201165d6e767c766a80d
Mahmut CAVDAR <mahmutcvdr@gmail.com>
2016-01-28 00:02:11 +0200

    add connection between pages

--- NLP/src/motorRecherche/XmlParser.java
+++ NLP/src/motorRecherche/XmlParser.java
@@ -1,7 +1,13 @@
 package motorRecherche;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import edu.jhu.nlp.wikipedia.*;
 
@@ -9,6 +15,8 @@ public class XmlParser {
 
 	private int count = 0;
 	private Hashtable<String, Integer> pageTable = new Hashtable<String, Integer>();
+	private Map<Integer, ArrayList> motPage = new HashMap<Integer, ArrayList>();
+	private Map<Integer, ArrayList<Integer>> liensPage = new HashMap<Integer, ArrayList<Integer>>();
 
 	private int lirePages() {
 
@@ -19,7 +27,7 @@ public class XmlParser {
 			wxsp.setPageCallback(new PageCallbackHandler() {
 				public void process(WikiPage page) {
 					count++;
-					pageTable.put(page.getTitle(), count);
+					pageTable.put(page.getTitle().trim(), count);
 				}
 			});
 
@@ -31,20 +39,62 @@ public class XmlParser {
 		return 0;
 	}
 
+	private int relationMotPage(String text, int count) {
+
+		String words[] = text.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			System.out.print(words[i] + "<<");
+		}
+		// System.out.println(text);
+		return 0;
+
+	}
+
+	private int trouverLiens() {
+
+		WikiXMLParser wxsp2 = WikiXMLParserFactory
+				.getSAXParser("C://Users//Mahmut//Desktop//frwiki-20151226-pages-articles-multistream.xml");
+		try {
+
+			wxsp2.setPageCallback(new PageCallbackHandler() {
+				public void process(WikiPage page2) {
+					Integer pagenum = pageTable.get(page2.getTitle().trim());
+					ArrayList<Integer> temp = new ArrayList<Integer>();
+					for (String element : page2.getLinks()) {
+						try {
+							temp.add(pageTable.get(element));
+						} catch (NullPointerException e) {
+							System.out.println("bos");
+						}
+					}
+					liensPage.put(pagenum, temp);
+				}
+			});
+
+			wxsp2.parse();
+		} catch (
+
+		Exception e)
+
+		{
+			e.printStackTrace();
+			return -1;
+		}
+		return 0;
+
+	}
+
 	public static void main(String[] args) {
 		final XmlParser main1 = new XmlParser();
-		// TODO Auto-generated method stub
-		// frwiki-20151226-pages-articles-multistream
+
 		System.out.println("return: " + main1.lirePages());
 		System.out.println(main1.pageTable.size());
 
-		Enumeration names;
-		String str;
-		names = main1.pageTable.keys();
-		System.out.println(main1.pageTable.size());
-		while (names.hasMoreElements()) {
-			str = (String) names.nextElement(); //
-			System.out.println(str + ": " + main1.pageTable.get(str));
+		main1.trouverLiens();
+		System.out.println("size: " + main1.liensPage.size());
+
+		for (Entry<Integer, ArrayList<Integer>> entry : main1.liensPage.entrySet()) {
+			System.out.println(entry.getKey() + "/" + entry.getValue().toString());
 		}
 
 	}
