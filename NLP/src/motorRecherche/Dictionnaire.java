6d744e9529b955e521e2f670ce8d67d0d21fe230
Mahmut CAVDAR <mahmutcvdr@gmail.com>
2016-01-27 11:58:28 +0200

    other files

--- NLP/src/motorRecherche/Dictionnaire.java
+++ NLP/src/motorRecherche/Dictionnaire.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 
@@ -16,23 +18,17 @@ import org.jsoup.select.Elements;
 public class Dictionnaire {
 
 	private List<String> Dictionary;
-	private List<String> SupprimeElements = Arrays.asList("le", "la", "les",
-			"un", "une", "Ã ", "en", "sa", "son", "ses", "ma", "mon", "mes",
-			"ta", "ton", "tes", "de", "du", "des", "au", "aux", "etc", "ce",
-			"cet", "cette", "ceux", "celui", "celle", "Ã§a", "pas", "rien",
-			"aucun");
+	Hashtable<String, Integer> balance = new Hashtable<String, Integer>();
+	private List<String> SupprimeElements = Arrays.asList("le", "la", "les", "un", "une", "à", "en", "sa", "son", "ses",
+			"ma", "mon", "mes", "ta", "ton", "tes", "de", "du", "des", "au", "aux", "etc", "ce", "cet", "cette", "ceux",
+			"celui", "celle", "ça", "pas", "rien", "auchun", "");
 
 	private String accentControl(String oldStr) {
 		String newStr = new String(oldStr);
-		newStr = newStr.replace('Ã ', 'a');
-		newStr = newStr.replace("Ã¨", "e");
-		newStr = newStr.replace("Ã©", "e");
-		newStr = newStr.replace("Ã¹", "u");
-		newStr = newStr.replace("Ã§", "c");
-		newStr = newStr.replace("Ã¯", "i");
-		newStr = newStr.replace("Ã´", "o");
-		newStr = newStr.replace("Ãª", "e");
-		newStr = newStr.replace("Ã¢", "a");
+		newStr = newStr.replace('à', 'a');
+		newStr = newStr.replace('è', 'e');
+		newStr = newStr.replace('é', 'e');
+		newStr = newStr.replace('ù', 'u');
 		return newStr;
 
 	}
@@ -42,9 +38,7 @@ public class Dictionnaire {
 		try {
 			// get all words
 			for (int cnt = 1; cnt <= 10; cnt++) {
-				doc = Jsoup.connect(
-						"https://fr.wiktionary.org/wiki/Wiktionnaire:10000-wp-fr-"
-								+ cnt + "000").get();
+				doc = Jsoup.connect("https://fr.wiktionary.org/wiki/Wiktionnaire:10000-wp-fr-" + cnt + "000").get();
 				Elements uls = doc.select("ul");
 				for (Element ul : uls) {
 					Elements lis = ul.select("li");
@@ -86,10 +80,24 @@ public class Dictionnaire {
 		d1.supprimeRedondance();
 		System.out.println(d1.Dictionary.size());
 		System.out.println(d1.Dictionary.get(1000));
-		Collections.sort(d1.Dictionary.subList(1, d1.Dictionary.size()));
+		Collections.sort(d1.Dictionary.subList(0, d1.Dictionary.size()));
 		System.out.println(d1.Dictionary.size());
 		System.out.println(d1.Dictionary.get(8000));
 
+		for (int i = 0; i < d1.Dictionary.size(); i++) {
+			d1.balance.put(d1.Dictionary.get(i), i);
+		}
+
+		Enumeration names;
+		String str;
+		names = d1.balance.keys();
+		System.out.println(d1.balance.size());
+		while (names.hasMoreElements()) {
+			str = (String) names.nextElement();
+			// System.out.println(str + ": " + d1.balance.get(str));
+		}
+
+		System.out.println(d1.balance.get("deux"));
 	}
 
 }
