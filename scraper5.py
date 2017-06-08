0e71d914cdb339eeda50025dfdd3f1a6df727ccc
Mahmut CAVDAR <mahmutcvdr@gmail.com>
2017-06-08 23:31:27 +0200

    added request with keyword - all results

--- scraper5.py
+++ scraper5.py
@@ -1,20 +1,32 @@
+# -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Selector
 import html2text
 import nltk
 from scrapy.http import Request
-
+import urllib
 
 #F = open("cabbar.txt","w") 
 
 
 from bs4 import BeautifulSoup
 class BrickSetSpider(scrapy.Spider):
+    cabbar = 'étoit'
     name = "brickset_spider"
-    start_urls = ['https://fr.wikisource.org/w/index.php?search=%C3%A9toit&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=18091&offset=0']
+    start_urls = ['https://fr.wikisource.org/w/index.php?search='+urllib.quote(cabbar)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=5000&offset=0']
 
 
     def parse(self, response):
+        SET_SELECTOR2 = '.results-info'
+	for cabbar in response.css(SET_SELECTOR2):
+	    aa = cabbar.extract()
+	    sel = Selector(text=aa,type="html")
+       	    za = (sel.xpath('//div[@class="results-info"]//@data-mw-num-results-total')).extract()
+	    for x in range(1,int(int(za[0])/5000)):
+	    	new_request = Request('https://fr.wikisource.org/w/index.php?search='+urllib.quote(self.cabbar)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit='+str(5000*x)+'&offset='+str(x*5000), callback=self.parse)
+            	yield new_request
+	
+
         SET_SELECTOR = '.mw-search-results'
         for brickset in response.css(SET_SELECTOR):
 	    aa = brickset.extract()
@@ -32,7 +44,7 @@ class BrickSetSpider(scrapy.Spider):
 	    aa = brickset.extract()
 	    sel2 = Selector(text=aa,type="html")
 	    soup2 = BeautifulSoup(aa)
-            F=open("cabbar.txt","a")
+            F=open(self.cabbar+".txt","a")
             F.write(soup2.text.encode('utf-8','ignore'))
 	    if(len(soup2.text.encode('utf-8','ignore'))<10):
 		F.write("problemmosssss")
