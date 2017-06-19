b3d2a0511e958c6b311699911beadf6c8453db64
Mahmut CAVDAR <mahmutcvdr@gmail.com>
2017-06-19 18:50:40 +0200

    little changes

--- scraper5.py
+++ scraper5.py
@@ -6,33 +6,31 @@ import nltk
 from scrapy.http import Request
 import urllib
 import re
-
+import sys
 
 from bs4 import BeautifulSoup
 class BrickSetSpider(scrapy.Spider):
-    cabbar = 'étoit'
+    word = str(sys.argv)[0]
     name = "brickset_spider"
-    start_urls = ['https://fr.wikisource.org/w/index.php?search='+urllib.quote(cabbar)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=5000&offset=0']
+    start_urls = ['https://fr.wikisource.org/w/index.php?search='+urllib.quote(word)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=5000&offset=0']
 
 
     def parse(self, response):
         SET_SELECTOR2 = '.results-info'
-	for cabbar in response.css(SET_SELECTOR2):
-	    aa = cabbar.extract()
-	    sel = Selector(text=aa,type="html")
-       	    za = (sel.xpath('//div[@class="results-info"]//@data-mw-num-results-total')).extract()
-	    for x in range(1,int(int(za[0])/5000)):
-	    	new_request = Request('https://fr.wikisource.org/w/index.php?search='+urllib.quote(self.cabbar)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit='+str(5000*x)+'&offset='+str(x*5000), callback=self.parse)
+	for rslt_info in response.css(SET_SELECTOR2):
+	    rslt_info_txt = rslt_info.extract()
+	    sel = Selector(text=rslt_info_txt,type="html")
+       	    result_number = (sel.xpath('//div[@class="results-info"]//@data-mw-num-results-total')).extract()
+	    for cnt in range(1,int(int(result_number[0])/5000)):
+	    	new_request = Request('https://fr.wikisource.org/w/index.php?search='+urllib.quote(self.word)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit='+str(5000*cnt)+'&offset='+str(cnt*5000), callback=self.parse)
             	yield new_request
 	
 
         SET_SELECTOR = '.mw-search-results'
         for brickset in response.css(SET_SELECTOR):
 	    aa = brickset.extract()
-	    #print sss
 	    sel = Selector(text=aa,type="html")
        	    urlst = (sel.xpath('//li//@href')).extract()
-	    #print urlst
 	    for url in urlst:
                 new_request = Request('https://fr.wikisource.org'+url, callback=self.parse2)
                 yield new_request
