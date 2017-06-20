d08dae3af48891ada9fbfd0d67f78dbe581f3fe9
Mahmut CAVDAR <mahmutcvdr@gmail.com>
2017-06-20 03:04:27 +0200

    resolve some bug and added argument pass system

--- scraper5.py
+++ scraper5.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
+
+# to run : scrapy runspider scraper5.py -a word='word' -s JOBDIR='your_folder'
+# example : scrapy runspider scraper5.py -a word=étoit -s JOBDIR=dir
+
 import scrapy
 from scrapy import Selector
 import html2text
-import nltk
 from scrapy.http import Request
 import urllib
-import re
 import sys
-
 from bs4 import BeautifulSoup
-class BrickSetSpider(scrapy.Spider):
-    word = str(sys.argv)[0]
-    name = "brickset_spider"
-    start_urls = ['https://fr.wikisource.org/w/index.php?search='+urllib.quote(word)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=5000&offset=0']
+
+
+class WikiSpider(scrapy.Spider):
+    name = "wikisource_spider"
+
+    def __init__(self, word='', domain=None, *args, **kwargs):
+	super(WikiSpider, self).__init__(*args, **kwargs)
+	self.word = word
+	self.start_urls = ['https://fr.wikisource.org/w/index.php?search='+urllib.quote(self.word)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=5000&offset=0']
 
 
     def parse(self, response):
         SET_SELECTOR2 = '.results-info'
-	for rslt_info in response.css(SET_SELECTOR2):
-	    rslt_info_txt = rslt_info.extract()
-	    sel = Selector(text=rslt_info_txt,type="html")
-       	    result_number = (sel.xpath('//div[@class="results-info"]//@data-mw-num-results-total')).extract()
-	    for cnt in range(1,int(int(result_number[0])/5000)):
-	    	new_request = Request('https://fr.wikisource.org/w/index.php?search='+urllib.quote(self.word)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit='+str(5000*cnt)+'&offset='+str(cnt*5000), callback=self.parse)
+	for cabbar in response.css(SET_SELECTOR2):
+       	    za = cabbar.xpath('//div[@class="results-info"]/@data-mw-num-results-total').extract()
+	    for x in range(1,int(int(za[0])/5000)+1):
+	    	new_request = Request('https://fr.wikisource.org/w/index.php?search='+urllib.quote(self.word)+'&title=Sp%C3%A9cial%3ARecherche&fulltext=Rechercher&limit=5000&offset='+str(x*5000), callback=self.parse)
             	yield new_request
 	
 
         SET_SELECTOR = '.mw-search-results'
         for brickset in response.css(SET_SELECTOR):
-	    aa = brickset.extract()
-	    sel = Selector(text=aa,type="html")
-       	    urlst = (sel.xpath('//li//@href')).extract()
+       	    urlst = brickset.xpath('//li//a/@href').extract()
 	    for url in urlst:
                 new_request = Request('https://fr.wikisource.org'+url, callback=self.parse2)
                 yield new_request
@@ -40,7 +42,7 @@ class BrickSetSpider(scrapy.Spider):
         for brickset in response.css(SET_SELECTOR):
 	    aa = brickset.extract()
 	    soup2 = BeautifulSoup(aa)
-            F=open(self.cabbar+".txt","a")
+            F=open(self.word+".txt","a")
             F.write(soup2.text.encode('utf-8','ignore').split('►')[-1])
             F.write("==============================================")
 	    if(len(soup2.text.encode('utf-8','ignore'))<10):
